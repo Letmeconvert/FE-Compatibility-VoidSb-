1e9acea67df10252917ec0c96091fbccc16bc6f8
Letmeconvert <42878168+Letmeconvert@users.noreply.github.com>
2018-09-20 16:46:14 -0700

    Update auto-updating.lua

--- auto-updating.lua
+++ auto-updating.lua
@@ -1,4 +1,4 @@
-local ScriptLink = [https://pastebin.com/raw/f1wyW4YW]
+local ScriptLink = [[https://pastebin.com/raw/giAmRZp0]]
 --//Do not touch anything below this line, you may break it.
 local FeSource = nil;pcall(function()FeSource = game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/WaverlyCole/FE-Compatibility-VoidSb-/master/translate.lua")end);
 local ScriptSource = nil;pcall(function()ScriptSource = game:GetService("HttpService"):GetAsync(ScriptLink)end);
